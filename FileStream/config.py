909ab7852a33266c852ec30272ba86c726df0a3f
venomdarshu7 <138948660+venomdarshu7@users.noreply.github.com>
2024-07-15 23:16:13 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -20,8 +20,8 @@ class Telegram:
     START_PIC = env.get('START_PIC', "https://telegra.ph/file/4bf29ca8a8e377ec09a8b.jpg")
     VERIFY_PIC = env.get('VERIFY_PIC', "https://telegra.ph/file/4bf29ca8a8e377ec09a8b.jpg")
     MULTI_CLIENT = False
-    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", '-1001586622410'))   # Logs channel for file logs
-    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", '-1001586622410'))   # Logs channel for user logs
+    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", '-1002066149181'))   # Logs channel for file logs
+    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", '-1002066149181'))   # Logs channel for user logs
     MODE = env.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
